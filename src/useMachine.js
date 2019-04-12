08f36e2e0c0f98d3ddcdac71bb858b1dae515528
degrecci <henrique.degrecci@upx.com>
2019-04-12 11:40:12 -0300

    Cria hook useMachine

---
+++ src/useMachine.js
@@ -0,0 +1,30 @@
+import { useState, useRef, useEffect } from 'react';
+
+import { interpret } from 'xstate';
+
+export function useMachine(machine) {
+  const [ current, setCurrent ] = useState(machine.initialState);
+
+  const serviceRef = useRef(null);
+
+  if (serviceRef.current === null ) {
+    serviceRef.current = interpret(machine).onTransition(state => {
+      if (state.changed) {
+        setCurrent(state);
+      }
+    })
+  }
+
+  const service = serviceRef.current;
+
+  useEffect(() => {
+    service.start();
+
+    return () => {
+      service.stop();
+    }
+  }, []);
+
+  return [current, service.send];
+}
+
