9579b2a9e22e92b067db59ec9dba488f5116ccd2
degrecci <henrique.degrecci@upx.com>
2019-05-24 10:41:11 -0300

    Cria state machine de checkin

---
+++ src/CheckIn/checkInMachine.js
@@ -0,0 +1,69 @@
+const changeIntervalToYearly = assign({
+  interval: 'yearly'
+});
+
+const changeIntervalToMonthly = assign({
+  interval: 'monthly'
+});
+
+const checkInMachine = {
+  id: 'checkin',
+  initial: 'loading',
+  context: { interval: 'yearly' },
+  states: {
+    loading: {
+      after: {
+        1000: 'plans.yearly',
+      },
+      on: {
+        RESOLVE: 'plans.yearly',
+        REJECT: 'loading',
+      }
+    },
+    plans: {
+      on: {
+        SELECT: 'confirmPlan',
+      },
+      states: {
+        initial: 'yearly',
+        yearly: {
+          onEntry: 'changeIntervalToYearly',
+          on: {
+            TOGGLE: 'monthly',
+          }
+        },
+        monthly: {
+          onEntry: 'changeIntervalToMonthly',
+          on: {
+            TOGGLE: 'yearly',
+          }
+        }
+      }
+    },
+    confirmPlan: {
+      on: {
+        CLOSE_PLAN: 'closePlan',
+        CANCEL: 'plans.yearly'
+      }
+    },
+    closePlan: {
+      on: {
+        RESOLVE: 'billing',
+        REJECT: 'plans.yearly',
+      }
+    },
+    billing: {
+      type: 'final'
+    }
+  }
+}
+
+const sideEffects = {
+  actions: {
+    changeIntervalToYearly,
+    changeIntervalToMonthly,
+  },
+  guards: {
+    chosedInterval: (context => context.interval === 'yearly' ? 'plans.yearly' : 'plans.montlhy')
+  }
+};
