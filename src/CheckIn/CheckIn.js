b3f2d8926f472988f7fa925e578f43a4511e95d2
degrecci <henrique.degrecci@upx.com>
2019-05-31 09:47:20 -0300

    Aplica todo state machine em render

--- src/CheckIn/CheckIn.js
+++ src/CheckIn/CheckIn.js
@@ -11,36 +11,56 @@ import {
 } from '../styles'
 
 const CheckIn = () => {
-  const machine = useMachine(checkInMachine, sideEffects);
+  const { send, state, context } = useMachine(checkInMachine, sideEffects);
 
-  console.log(machine);
+  const confirmPlan = () => send('SELECT');
+
+  const resolvePlanConfirmation = () => new Promise((resolve) => setTimeout(resolve, 500));
+
+  state.matches('closePlan') &&
+    resolvePlanConfirmation()
+      .then(() => send('RESOLVE'))
+      .catch(() => send('REJECT'))
 
   return (
     <div>
-      {machine.state.matches('loading') ? `Loading`
-      : (
+      {state.matches('loading') && `Loading`}
+      {state.matches('plans') && (
         <>
-          <Toggle type="checkbox"/>
-          {` ${machine.context.interval}`}
+          <Toggle type="checkbox" onClick={() => send('TOGGLE')}/>
+          {` ${context.interval}`}
 
           <Cards>
             <Card>
               <H1>Plano 1</H1>
-              <Button>Contratar</Button>
+              <Button onClick={confirmPlan}>Contratar</Button>
             </Card>
             <Card>
               <H1>Plano 2</H1>
-              <Button>Contratar</Button>
+              <Button onClick={confirmPlan}>Contratar</Button>
             </Card>
           </Cards>
         </>
       )}
+      {state.matches('confirmPlan') && (
+        <Card>
+          <H1>Confirm your plan</H1>
+          <Button onClick={() => send('CANCEL')}>Cancelar</Button>
+          <Button onClick={() => send('CLOSE_PLAN')}>Confirmar</Button>
+        </Card>
+      )}
+      {state.matches('closePlan') &&
+        <Card>
+          <H1>Confirmando Plano</H1>
+        </Card>
+      }
+      {state.matches('billing') &&
+        <Card>
+          <H1>Billing Page</H1>
+        </Card>
+      }
     </div>
   );
 }
-
-CheckIn.propTypes = {
-
-};
 
 export default CheckIn;
